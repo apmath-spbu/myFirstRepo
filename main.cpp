f3886bf6a665577db7078ac9ca449379399edecf
su23 <apmath.spbu@gmail.com>
2020-10-31 11:53:10 +0300

    added welcome message to user

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-	cout << "Hello world";
+	string s;
+	cout << "What's your name?";
+	cin >> s;
+	cout << "Hello " << s;
 	return 0;	
 }
